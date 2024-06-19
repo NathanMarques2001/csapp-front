1aa5bbe9c2a19f24d30b20e761406bda360177f8
dietze <devlojadosapo@gmail.com>
2024-06-19 17:38:20 -0300

    update cadastro de cliente

--- src/paginas/cadastro-cliente/index.jsx
+++ src/paginas/cadastro-cliente/index.jsx
@@ -14,11 +14,11 @@ export default function CadastroCliente() {
               <p>Campos com <span class="required">*</span> são obrigatórios.</p>
               <div class="form-group">
                 <label for="nome">Nome <span class="required">*</span></label>
-                <input type="text" id="nome" required />
+                <input type="text" id="nome" required placeholder="Digite o nome completo"/>
               </div>
               <div class="form-group">
                 <label for="cpf-cnpj">CPF/CNPJ <span class="required">*</span></label>
-                <input type="text" id="cpf-cnpj" required />
+                <input type="text" id="cpf-cnpj" required placeholder="Digite o CPF ou CNPJ"/>
               </div>
               <div class="form-group">
                 <label for="relacionamento">Relacionamento</label>
@@ -29,11 +29,11 @@ export default function CadastroCliente() {
               </div>
               <div class="form-group">
                 <label for="segmento">Segmento <span class="required">*</span></label>
-                <input type="text" id="segmento" required />
+                <input type="text" id="segmento" required placeholder="Digite o ramo do cliente"/>
               </div>
               <div class="form-group">
                 <label for="nps">NPS</label>
-                <input type="text" id="nps" />
+                <input type="text" id="nps" placeholder="Digite o NPS do cliente"/>
               </div>
             </div>
             <div id="cadastro-cliente-img-div">
@@ -45,69 +45,75 @@ export default function CadastroCliente() {
               <h2>Gestor de Contrato</h2>
               <div class="form-group">
                 <label for="nome-contrato">Nome <span class="required">*</span></label>
-                <input type="text" id="nome-contrato" required />
+                <input type="text" id="nome-contrato" required placeholder="Digite o nome completo"/>
               </div>
               <div class="form-group">
                 <label for="email-contrato">Email <span class="required">*</span></label>
-                <input type="email" id="email-contrato" required />
+                <input type="email" id="email-contrato" required placeholder="Digite seu endereço de email"/>
               </div>
+              <div className="date-container">
               <div class="form-group">
                 <label for="aniversario-contrato">Aniversário</label>
                 <input type="date" id="aniversario-contrato" />
               </div>
               <div class="form-group">
                 <label for="telefone1-contrato">Telefone 1</label>
-                <input type="text" id="telefone1-contrato" />
+                <input type="text" id="telefone1-contrato" placeholder="Primeiro contato"/>
               </div>
               <div class="form-group">
                 <label for="telefone2-contrato">Telefone 2</label>
-                <input type="text" id="telefone2-contrato" />
+                <input type="text" id="telefone2-contrato" placeholder="Segundo contato"/>
+              </div>
               </div>
             </div>
             <div>
               <h2>Gestor de Chamados</h2>
               <div class="form-group">
                 <label for="nome-chamados">Nome <span class="required">*</span></label>
-                <input type="text" id="nome-chamados" required />
+                <input type="text" id="nome-chamados" required placeholder="Digite o nome completo"/>
               </div>
               <div class="form-group">
                 <label for="email-chamados">Email <span class="required">*</span></label>
-                <input type="email" id="email-chamados" required />
+                <input type="email" id="email-chamados" required placeholder="Digite seu endereço de email"/>
               </div>
+              <div className="date-container">
               <div class="form-group">
                 <label for="aniversario-chamados">Aniversário</label>
                 <input type="date" id="aniversario-chamados" />
               </div>
               <div class="form-group">
                 <label for="telefone1-chamados">Telefone 1</label>
-                <input type="text" id="telefone1-chamados" />
+                <input type="text" id="telefone1-chamados" placeholder="Primeiro contato"/>
               </div>
               <div class="form-group">
                 <label for="telefone2-chamados">Telefone 2</label>
-                <input type="text" id="telefone2-chamados" />
+                <input type="text" id="telefone2-chamados" placeholder="Segundo contato"/>
+              </div>
               </div>
             </div>
             <div>
               <h2>Gestor Financeiro</h2>
               <div class="form-group">
                 <label for="nome-financeiro">Nome <span class="required">*</span></label>
-                <input type="text" id="nome-financeiro" required />
+                <input type="text" id="nome-financeiro" required placeholder="Digite o nome completo"/>
               </div>
               <div class="form-group">
                 <label for="email-financeiro">Email <span class="required">*</span></label>
-                <input type="email" id="email-financeiro" required />
+                <input type="email" id="email-financeiro" required placeholder="Digite seu endereço de email"/>
               </div>
+              <div className="date-container">
               <div class="form-group">
                 <label for="aniversario-financeiro">Aniversário</label>
                 <input type="date" id="aniversario-financeiro" />
               </div>
               <div class="form-group">
                 <label for="telefone1-financeiro">Telefone 1</label>
-                <input type="text" id="telefone1-financeiro" />
+                <input type="text" id="telefone1-financeiro" placeholder="Primeiro contato"/>
               </div>
               <div class="form-group">
                 <label for="telefone2-financeiro">Telefone 2</label>
-                <input type="text" id="telefone2-financeiro" />
+                <input type="text" id="telefone2-financeiro" placeholder="Segundo contato"/>
+              </div>
               </div>
             </div>
           </form>
