e2b8fcbc6d80e8a7dfac88a395402a79a8781b37
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2025-03-16 22:08:34 -0300

    feat: corrigir URL base da API de email

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 class Email {
   // PRD
-  static baseUrl = "https://csapp.prolinx.com.br/email";
+  static baseUrl = "https://csapp.prolinx.com.br/email/email";
   // DEV
   //static baseUrl = "http://localhost:9090/email";
   constructor() {
