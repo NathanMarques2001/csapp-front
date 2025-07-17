434283460bc12e8ab20c8000bf376ff186725a75
NathanArch <nathanmarques20@yahoo.com>
2025-07-17 00:01:40 -0300

    style: popup importa contratos

--- src/componetes/pop-up-importa-contrato/index.jsx
+++ src/componetes/pop-up-importa-contrato/index.jsx
@@ -12,11 +12,24 @@ export default function PopUpImportaContratos({
     <div id="background-pop-up-importa-contratos">
       <div id="container-pop-up-importa-contratos">
         <h1 id="titulo-pop-up-importa-contratos">Importar Contratos</h1>
-        <button onClick={baixarModelo} className="contratos-botao">
+        <p>
+          Clique em "Baixar Modelo" para baixar um modelo de arquivo a ser
+          importado.
+        </p>
+        <button
+          onClick={baixarModelo}
+          className="pop-up-importa-contratos-botao"
+          id="contratos-botao-add"
+        >
           Baixar Modelo
         </button>
+        <p>
+          Clique em "Selecionar Excel" para selecionar o arquivo a ser
+          importado.
+        </p>
         <form
           id="form-pop-up-importa-contratos"
+          className="pop-up-importa-contratos-botao"
           onSubmit={async (e) => {
             e.preventDefault();
             const file = e.target.file.files[0];
@@ -45,7 +58,7 @@ export default function PopUpImportaContratos({
             }
           }}
         >
-          <label htmlFor="file-upload" className="contratos-botao">
+          <label htmlFor="file-upload" id="pop-up-importa-contratos-label">
             Selecionar Excel
           </label>
           <input
@@ -61,9 +74,11 @@ export default function PopUpImportaContratos({
             }}
           />
         </form>
+        <div id="pop-up-importa-contratos-linha" />
         <button
           onClick={() => setMostrarModalImportacao(false)}
-          className="contratos-botao"
+          className="pop-up-importa-contratos-botao"
+          id="popup-botao-cancelar"
         >
           Fechar
         </button>
